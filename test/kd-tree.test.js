0a5945fa5bc88f2aa03d9085398d5053e1bb5bd2
Al Nejati <alireza.n.j@gmail.com>
2023-10-19 19:02:37 +1300

    Initial commit.

---
+++ test/kd-tree.test.js
@@ -0,0 +1,66 @@
+import chai from 'chai';
+import kdTree, { viewDepthSort } from '../lib/kd-tree.js';
+import IndexedPointArray from '../lib/pointarray.js';
+
+const { expect } = chai;
+
+describe('kdTree', () => {
+  it('should create a tree correctly', () => {
+    const iparr = new IndexedPointArray(
+      new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
+      new Float32Array([17.2,  0.5, 24.0,  2.0, 10.5,  2.5, 19.5,  4.5]),
+      new Float32Array([13.0, 11.0, 13.0, 12.5, 19.7,  1.8, 24.5, 20.0]),
+    );
+    
+    const tree = kdTree(iparr, 2);
+
+    expect(iparr.idx.find((x)=>(x==2)) <= 4);
+    expect(iparr.idx.find((x)=>(x==3)) <= 4);
+    expect(iparr.idx.find((x)=>(x==4)) <= 4);
+    expect(iparr.idx.find((x)=>(x==5)) <= 4);
+
+    expect(tree.d).to.equal(0);
+    expect(tree.val).to.equal(3.5);
+    expect(tree.cleft.d).to.equal(1);
+    expect(tree.cleft.val).to.equal(2.5);
+    expect(tree.cleft.cleft.range).to.deep.equal([0, 1]);
+    expect(tree.cleft.cright.range).to.deep.equal([2, 3]);
+    expect(tree.cright.d).to.equal(1);
+    expect(tree.cright.val).to.equal(4.5);
+    expect(tree.cright.cleft.range).to.deep.equal([4, 5]);
+    expect(tree.cright.cright.range).to.deep.equal([6, 7]);
+  });
+
+  it.skip('should add a new batch of points to the kdtree correctly.', () => {
+    const iparr = new IndexedPointArray(
+      new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
+      new Float32Array([17.2,  0.5, 24.0,  2.0, 10.5,  2.5, 19.5,  4.5]),
+      new Float32Array([13.0, 11.0, 13.0, 12.5, 19.7,  1.8, 24.5, 20.0]),
+    );
+    const tree = kdTree(iparr, 2);
+    
+    // TODO
+  });
+
+
+  it('should sort the tree by depth correctly', () => {
+    const iparr = new IndexedPointArray(
+      new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
+      new Float32Array([17.2,  0.5, 24.0,  2.0, 10.5,  2.5, 19.5,  4.5]),
+      new Float32Array([13.0, 11.0, 13.0, 12.5, 19.7,  1.8, 24.5, 20.0]),
+    );
+    const tree = kdTree(iparr, 2);
+
+    viewDepthSort(tree, {x: 3, y: -3, z: -3});
+
+    expect(tree.cleft.val).to.equal(4.5);
+    expect(tree.cleft.cleft.range).to.deep.equal([6, 7]);
+    expect(tree.cleft.cright.range).to.deep.equal([4, 5]);
+    expect(tree.cright.d).to.equal(1);
+    expect(tree.cright.val).to.equal(2.5);
+    expect(tree.cright.cleft.range).to.deep.equal([2, 3]);
+    expect(tree.cright.cright.range).to.deep.equal([0, 1]);
+
+  });
+
+});
