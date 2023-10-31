5e5c3a5b2b96a545f19d24710f652b07752e38d9
Al Nejati <alireza.n.j@gmail.com>
2023-11-01 12:33:47 +1300

    Change to ES modules and do in-browser tests

--- test/kd-tree.test.js
+++ test/kd-tree.test.js
@@ -29,15 +29,6 @@ describe('kdTree', () => {
         expect(tree.cright.val).to.equal(4.5);
         expect(tree.cright.cleft.range).to.deep.equal([4, 6]);
         expect(tree.cright.cright.range).to.deep.equal([6, 8]);
-
-        // let perm = depthFirstCollect(tree, iparr.idx, 0);
-
-        // let iparr_new = permutePointArray(iparr, perm);
-
-        // console.log(iparr_new.x)
-        // console.log(iparr_new.y)
-        // console.log(iparr_new.z)
-        // TODO: create new verts, cov matrices, etc. from idx
     });
 
     it.skip('should add a new batch of points to the kdtree correctly.', () => {
@@ -52,25 +43,6 @@ describe('kdTree', () => {
     });
 
 
-    it('should sort the tree by depth correctly', () => {
-        const iparr = new IndexedPointArray(
-            new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
-            new Float32Array([17.2,  0.5, 24.0,  2.0, 10.5,  2.5, 19.5,  4.5]),
-            new Float32Array([13.0, 11.0, 13.0, 12.5, 19.7,  1.8, 24.5, 20.0]),
-        );
-        const tree = kdTree(iparr, 2);
-
-        viewDepthSort(tree, {x: 3, y: -3, z: -3});
-
-        expect(tree.cleft.val).to.equal(4.5);
-        expect(tree.cleft.cleft.range).to.deep.equal([6, 8]);
-        expect(tree.cleft.cright.range).to.deep.equal([4, 6]);
-        expect(tree.cright.d).to.equal(1);
-        expect(tree.cright.val).to.equal(2.5);
-        expect(tree.cright.cleft.range).to.deep.equal([2, 4]);
-        expect(tree.cright.cright.range).to.deep.equal([0, 2]);
-    });
-
     it('should sort the tree by reverse depth correctly', () => {
         const iparr = new IndexedPointArray(
             new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
@@ -79,14 +51,27 @@ describe('kdTree', () => {
         );
         const tree = kdTree(iparr, 2);
 
-        viewDepthSort(tree, {x: 3, y: -3, z: -3}, 'reverse');
+        let idx = viewDepthSort(tree, {x: 3, y: -3, z: -3}, 'reverse');
+
+        expect(idx[0]).to.deep.equal([6, 8]);
+        expect(idx[1]).to.deep.equal([4, 6]);
+        expect(idx[2]).to.deep.equal([2, 4]);
+        expect(idx[3]).to.deep.equal([0, 2]);
+    });
+
+    it('should sort the tree by depth correctly', () => {
+        const iparr = new IndexedPointArray(
+            new Float32Array([23.0, 12.5,  3.0,  3.5,  3.0,  1.0, 21.7,  4.0]),
+            new Float32Array([17.2,  0.5, 24.0,  2.0, 10.5,  2.5, 19.5,  4.5]),
+            new Float32Array([13.0, 11.0, 13.0, 12.5, 19.7,  1.8, 24.5, 20.0]),
+        );
+        const tree = kdTree(iparr, 2);
+
+        let idx = viewDepthSort(tree, {x: 3, y: -3, z: -3});
         
-        expect(tree.cleft.val).to.equal(2.5);
-        expect(tree.cleft.cleft.range).to.deep.equal([0, 2]);
-        expect(tree.cleft.cright.range).to.deep.equal([2, 4]);
-        expect(tree.cright.d).to.equal(1);
-        expect(tree.cright.val).to.equal(4.5);
-        expect(tree.cright.cleft.range).to.deep.equal([4, 6]);
-        expect(tree.cright.cright.range).to.deep.equal([6, 8]);
+        expect(idx[0]).to.deep.equal([0, 2]);
+        expect(idx[1]).to.deep.equal([2, 4]);
+        expect(idx[2]).to.deep.equal([4, 6]);
+        expect(idx[3]).to.deep.equal([6, 8]);
     });    
 });
